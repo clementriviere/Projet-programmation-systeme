73d1cddf9e7fd994b4d82dc40505148b19998406
BaptisteLauras <baptiste.lauras@viacesi.fr>
2018-12-06 15:01:06 +0100

    Stub code for Service Layer

--- Projet programmation système/Model/Service/Equipment.cs
+++ Projet programmation système/Model/Service/Equipment.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Model.Data;
 using System.Threading.Tasks;
 
-namespace Model.Business.Mapper
+
+namespace Model.Service
 {
     class Equipment
     {
+        public Model.Data.Context Context;
+
+        public Post()
+        {
+            Context = new Context();
+        }
+
+
+
+        public void Add(Business.Post Post)
+        {
+            
+        }
+
+
+        public void Delete(int id)
+        {
+           
+        }
+
+
+        public void Update(Business.Post Post)
+        {
+
+            
+        }
+
+
+        public List<Business.Post> Get()
+        {
+            return null;
+
+        }
+
+
+        public Business.Post Get(int Id)
+        {
+            return null;
+        }
     }
 }
--- Projet programmation système/Model/Service/Ingredient.cs
+++ Projet programmation système/Model/Service/Ingredient.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model.Data;
 
 namespace Model.Service
 {
     class Ingredient
     {
+        public Model.Data.Context Context;
+
+        public Ingredient()
+        {
+            Context = new Context();
+        }
+
+
+
+        public void Add(Business.Ingredient Ingredient)
+        {
+
+        }
+
+
+        public void Delete(int id)
+        {
+
+        }
+
+
+        public void Update(Business.Ingredient Ingredient)
+        {
+
+
+        }
+
+
+        public List<Business.Ingredient> Get()
+        {
+            return null;
+
+        }
+
+
+        public Business.Ingredient Get(int Id)
+        {
+            return null;
+        }
     }
 }
