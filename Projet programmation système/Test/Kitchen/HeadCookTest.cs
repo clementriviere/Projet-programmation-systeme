0f3f213df21f66520c04f5a26a7fec3386900ac0
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2018-12-07 11:12:38 +0100

    Fixing things, still dying inside

---
+++ Projet programmation système/Test/Kitchen/HeadCookTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class HeadCookTest
+    {
+        [TestMethod]
+        public void DivideTaskTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckStockAndDeleteDishTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckStockTest()
+        {
+
+        }
+    }
+}
---
+++ Projet programmation système/Test/Room/WaiterTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Room
+{
+    [TestClass]
+    public class WaiterTest
+    {
+        [TestMethod]
+        public void ServeClientTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ClearTableTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PutDishInDirtyStockTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void HelpOtherWaiterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PutBreadAndWaterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void WaitFoodTest()
+        {
+
+        }
+    }
+}
