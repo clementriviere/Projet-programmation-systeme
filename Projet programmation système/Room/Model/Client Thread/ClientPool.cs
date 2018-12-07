2f157db30f9bd1220bbfb29db23cd022edaf45ed
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2018-12-07 04:42:42 +0100

    Stub test retry

---
+++ Projet programmation système/Room/Model/Client Thread/ClientPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Model
+{
+    class ClientPool
+    {
+        public void AddClient(ClientThread client)
+        {
+
+        }
+
+        public void RemoveClient(ClientThread client)
+        {
+
+        }
+
+        public Boolean UserExist(ClientThread client)
+        {
+            /* A changer */ return true; /* A changer */
+        }
+    }
+}
