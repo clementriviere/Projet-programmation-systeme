5a0e553fe551a70c92f0f56f9caab25aa29178bd
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2018-12-10 15:54:05 +0100

    on est pas passé loin

---
+++ Projet programmation système/Projet programmation système/Kitchen/Model/IKitchen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_programmation_système.Kitchen.Mode
+{
+    interface IKitchen
+    {
+        int Width { get; }
+        int Height { get; }
+        int ElementCount { get; }
+        int EquipementCount { get; }
+        List<IElement> Equipements { get; set; }
+        List<IElement> Elements { get; set; }
+
+        void AddElement(IElement element);
+        void RemoveElement(IElement motion);
+        void AddEquipement(IEquipement equipement);
+        void RemoveEquipement(IEquipement equipement);
+    }
+}
