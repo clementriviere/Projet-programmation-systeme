e6bcb1f81ec2b4ff71b893bca00665c701d9e5bb
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2018-12-10 15:54:59 +0100

    fiou

---
+++ Projet programmation système/Room/Model/Employee/HeadWaiter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Model
+{
+    class HeadWaiter
+    {
+        public HeadWaiter()
+        {
+
+        }
+
+        public void PlaceClient(Table table) //Comment savoir quel client est assigné ? Nécessaire ?
+        {
+
+        }
+
+        public void GiveMenu(Menu menu)
+        {
+
+        }
+
+        /*public List<Menu> TakeCommand(List<Recipe> command)
+        {
+                    Reflechir à une solution pour les recipe, dépendances, references par rapport à l'assembly ???
+        }*/
+
+        public void GiveCommandToHeadCook(List<Menu> command)
+        {
+
+        }
+
+        public void HelpOtherHeadWaiter(HeadWaiter headwaiter)
+        {
+
+        }
+
+        public Table SetUpTable()
+        {
+            /* A changer */ return null; /* A changer */
+        }
+    }
+}
---
+++ Projet programmation système/UnitTestProject1/Room/RoomClerkTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.Room
+{
+    [TestClass]
+    public class RoomClerkTest
+    {
+        [TestMethod]
+        public void ServeBreadAndDrinksTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CheckIfClientNeedSomethingTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void BecomeWaiterTest()
+        {
+
+        }
+    }
+}
