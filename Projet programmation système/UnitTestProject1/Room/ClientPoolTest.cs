5251b0cb3ad5fa7ee3ddabde53cd6003338ad650
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2018-12-10 15:47:52 +0100

    oopsie

---
+++ Projet programmation système/UnitTestProject1/Room/ClientPoolTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.Room
+{
+    [TestClass]
+    public class ClientPoolTest
+    {
+        [TestMethod]
+        public void AddClientTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void RemoveClientTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void UserExistTest()
+        {
+
+        }
+    }
+}
